7d3de5ce3c1716156c46544fbf732361cc1dbe38
shashank-bhojappa <shashank.bhojappa1989@gmail.com>
2022-02-06 23:30:17 +0530

    Added the header section

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route,Routes } from 'react-router-dom';
 import './App.css';
 import ShopPage from './pages/shop/shop.component';
 import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+    <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage/>} />
