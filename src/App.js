469e8475c064c13255cd007cb1d0b4a74598b439
shashank-bhojappa <shashank.bhojappa1989@gmail.com>
2022-02-07 19:06:03 +0530

    added sig-in page and custom buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import ShopPage from './pages/shop/shop.component';
 import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage/>} />
+        <Route path='/signin' element={<SignInAndSignUpPage/>} />
       </Routes>    
     </div>
   );
